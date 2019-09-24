1d14a85d90857b5aaca8751bd92eadd0c8e1f2a9
Pengzhou Tang <ptang@pivotal.io>
2019-09-24 04:22:42 -0400

    Support for parallel grouping sets
    We used to support grouping sets in one worker only, this PR
    want to support parallel grouping sets in multiple workers.
    
    In the first stage, the partial aggregates are performed by
    multiple workers, each worker perform the aggregates on all
    grouping sets, meanwile, a grouping set id is attached to
    the tuples of first stage to identify which grouping set the
    tuple belongs to. In the final stage, the gathered tuples are
    dispatched to specified grouping set according to the
    additional set id and then perform combine aggregates per
    grouping set. We don't use GROUPING() func to identify the
    grouping set because a sets may contain duplicate grouping
    set.
    
    Some changes are also made by executor in final stage:
    
    For AGG_HASHED strategy, all grouping sets still perform
    combine aggregates in phase 0, the only difference is that
    only one group is selected in final stage, so we need to
    skip those un-selected groups.
    
    For AGG_MIXED strategy, phase 0 now also need to do its
    own aggregate now.
    
    For AGG_SORTED strategy, rollup will be expanded, eg:
    rollup(<c1, c2>, <c1>, <>) is expanded to three rollups:
    rollup(<c1, c2>), rollup(<c1>) and rollup(<>). so tuples
    can be dispatched to those three phases and do aggregate
    then.

--- src/include/nodes/pathnodes.h
+++ src/include/nodes/pathnodes.h
@@ -1674,6 +1674,7 @@ typedef struct GroupingSetData
 {
 	NodeTag		type;
 	List	   *set;			/* grouping set as list of sortgrouprefs */
+	int			grpsetId;			/* unique grouping set identifier */
 	double		numGroups;		/* est. number of result groups */
 } GroupingSetData;
 
@@ -1699,6 +1700,7 @@ typedef struct GroupingSetsPath
 	AggStrategy aggstrategy;	/* basic strategy */
 	List	   *rollups;		/* list of RollupData */
 	List	   *qual;			/* quals (HAVING quals), if any */
+	AggSplit   aggsplit;
 } GroupingSetsPath;
 
 /*
